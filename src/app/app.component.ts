063433db7d19cdeb92b145fc05622474be700387
benny <benpakmnz@gmail.com>
2021-04-18 01:56:59 +0300

    form changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { product } from './Modals/interfaces';
 import {products} from '../assets/mock-data';
 
@@ -7,7 +10,7 @@ import {products} from '../assets/mock-data';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-store';
   productsList: product[] = [];
   productSelected: product = { 
@@ -19,12 +22,44 @@ export class AppComponent {
     thumbnailImage: "",
     creationDate: null};
 
+  saveChangesDisabled: boolean = false;
+  selectedProductName: string = "";
+  isProductUpdate: boolean = false;
+  productForm: FormGroup;
+
+  ngOnInit(){
+  }
+
 constructor(){
   this.productsList = products;
 }
 
   productSelection(productId:number){
-    this.productSelected = this.productsList[productId];
-    console.log(this.productSelected)
+    this.productSelected = Object.assign({},this.productsList[productId]);
+    this.selectedProductName = this.productsList[productId].name;
+
+    this.productForm = new FormGroup({
+      'productName': new FormControl(this.productSelected.name, Validators.required),
+      'productDescription': new FormControl(this.productSelected.description),
+      'productPrice': new FormControl(this.productSelected.price, [Validators.min(10),Validators.required])
+    })
+  }
+
+  onSubmit(){
+    console.log(this.productForm)
+  }
+
+  sortlistHandler(event){
+    console.log(event)
+    this.productsList.sort((a,b) => {
+      if ( a.name < b.name ){
+        return 1;
+      }
+      if ( a.name > b.name ){
+        return -1;
+      }
+      return 0;
+    }
+    )
   }
 }
