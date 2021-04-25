35d41a32bb58e9cf25d4c724ab03ce8a2040e8c3
benpakmnz <benpakmnz@gmail.com>
2021-04-25 11:50:31 +0300

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductFormComponent } from './components/product-form/product-form.com
 import { ProductsListComponent } from './components/products-list/products-list.component';
 
 const routes: Routes = [
+  {path: '' , component: ProductsListComponent},
   {path: 'products', component: ProductsListComponent, children: [
     {path: ':id', component: ProductFormComponent}
   ]},
